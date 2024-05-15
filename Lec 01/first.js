f01273589292f6ba6d5e387b3e89952188c72ef1
alishba-naseer <rananaseer524@gmail.com>
2024-05-15 19:16:09 +0500

    Add variables

--- Lec 01/first.js
+++ Lec 01/first.js
@@ -1,1 +1,15 @@
- console.log("Apna college")
+ console.log("Welcome to Smart Coders!")
+ console.log("Hello Guys")
+ console.log("I love Javascript!")
+
+fullName =("Alishba Naseer")
+console.log("fullName")
+
+fullName =("Alishba Naseer")
+console.log(fullName)
+
+age =(24)
+console.log(age)
+
+price =(99.99)
+console.log (price)
