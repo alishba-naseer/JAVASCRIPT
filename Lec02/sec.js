6c40512a92c10659d4d108180f8e7b9fdeff544c
alishba-naseer <rananaseer524@gmail.com>
2024-05-23 17:58:25 +0500

    Add work

---
+++ Lec02/sec.js
@@ -0,0 +1,94 @@
+//   Arithmetic operators
+
+let a = 5; 
+let b = 2;
+
+console.log("a =", a ," & b =",b );
+console.log("a + b =", a + b);
+console.log("a - b =", a - b);
+//                        MULIPLICATION
+console.log("a * b =", a * b);
+//                        DIVISION
+console.log("a / b =", a / b);
+//                        MODULUS
+console.log("a % b =", a % b);
+//                        EXPONENTIATION
+console.log("a ** b =", a ** b);
+
+/*                       -> UNARY OPERATOR
+                            1) INCREMENT ++    */
+
+// a = a + 1;
+
+// a++
+// console.log("a =", a); 
+
+/*                          2)DECREMENT --     */
+a--
+console.log("a =", a); 
+
+
+//                           POST PRE CONCEPT
+
+let c = 6;
+let d = 3;
+console.log("c =", c ," &  d =", d );
+
+console.log("c++ =", c++);
+console.log("c =", c);              //post
+
+// console.log("++c =", ++c);
+// console.log("c =", c);          //pre
+
+console.log("d-- =", d--);
+console.log("d =", d);             //post
+
+// console.log("--d =", --d );
+// console.log("d =", d);          //pre
+
+//                               ASSIGNMENT OPERATORS
+
+let e = 5;
+let f = 8;
+console.log("e =", e, " &  f =", f);
+
+// e += 4;
+// console.log("e +=  =", e);
+
+// f -= 4;
+// console.log("f -=   =", f);
+
+// e *= 4;
+// console.log("e *=  =", e);
+
+// f /= 4;
+// console.log("f -=   =", f);
+
+e %= 4;
+console.log("e %=   =", e);
+
+f **= 4;
+console.log("f **=   =", f);
+
+//                                 COMPARISION OPERATORS
+ 
+let g = 5;
+let h = 4;
+let i = "4";
+
+console.log("g =", g , "  h =", h , " &  i =", i);
+console.log("5 == 4", g == h);
+console.log("5 != 4", g != h);
+console.log("h == i", h == i);
+console.log("h === i", h === i);
+console.log("h !== i", h !== i);
+console.log("5 > 4 ", g > h);
+console.log("5 < 4 ", g < h);
+console.log("5 >= 4 ", g >= h);
+console.log("5 <= 4 ", g <= h);
+
+//                                     LOGIC OPERATORS
+
+
+
+
