8341c428c34cbaad92bc1f0c2e40d8d9bc9b84bd
alishba-naseer <rananaseer524@gmail.com>
2024-05-29 11:42:17 +0500

    Add work

---
+++ Lec 03/script.js
@@ -0,0 +1,33 @@
+
+//?                                            PRINT 1 TO 5
+
+for (count = 1; count <= 5; count++){
+    console.log("apna college");
+}
+console.log("loop ha ended");
+
+//?                                        CALCULATE SUM 1 TO N
+
+let sum = 0;
+let n = 1000;
+for (let i = 1; i <= n; i++){
+    sum = sum + i;
+}
+console.log("sum =", sum);
+console.log("loop has ended");
+
+//TODO                                           WHILE LOOP
+
+let i = 1;
+while(i <= 10){
+    console.log("Apna college");
+    i++ ; 
+}
+
+//TODO                                          DO-WHILE LOOP
+
+let a= 20;
+do{
+    console.log("Apna college");
+    a++;
+} while(a <= 10);
